bdb01d35ffc69ef219a17cd3bcfc389d57b25016
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-16 12:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function getFormvalue() {
-    //Write your code here
+// function getFormvalue() {
+//     //Write your code here
 
+// }
+// script.js
+
+function getFormvalue() {
+  // Get the input values from the form
+  var firstName = document.getElementById("first-name").value;
+  var lastName = document.getElementById("last-name").value;
+
+  // Concatenate the first and last name
+  var fullName = firstName + " " + lastName;
+
+  // Show an alert with the first and last name
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
